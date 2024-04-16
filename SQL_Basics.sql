01832fb1bbc532451c4e290dec276e552f95b73a
kavyasriarum <147021791+kavyasriarum@users.noreply.github.com>
2024-04-15 21:13:49 -0400

    Add files via upload

---
+++ SQL_Basics.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Kavyaworkspace; -- command helps to create a DB
+USE Kavyaworkspace; -- switches to newly created database
+CREATE TABLE Persons (  -- command helps to create a table
+    PersonID int, -- specifing column name and its data type 
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+-- DROP TABLE Persons; --command to drop the table
+
+   
